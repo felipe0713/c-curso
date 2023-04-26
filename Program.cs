ad145e19687632dcdf9678b5aeaacd50540841aa
felipe0713 <102712784+felipe0713@users.noreply.github.com>
2023-04-26 14:58:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Ejercicio 1
+Console.WriteLine("Introduzca su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Buen diá " + nombre);
+
+
+// Ejercicio 2
+Console.WriteLine("Introduzca su edad ");
+string edad = Console.ReadLine();
+Console.WriteLine("su edad es de " + edad + "años");
